20df59929962552c07ede64b927a8de9de8289cd
FaziletOe <61784442+FaziletOe@users.noreply.github.com>
2020-03-04 17:00:02 +0100

    BugFix:Button

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,8 +10,7 @@ class QuizQuestion extends Component{
            <p>{this.props.quiz_question.instruction_text}</p>
          </section>
          <section className="buttons">
-           <ul>
-             <QuizQuestionButton button_text = {this.props.quiz_question.answer_options[0]} />
+           <ul><QuizQuestionButton button_text = {this.props.quiz_question.answer_options[0]} />
            </ul>
          </section>
        </main>
